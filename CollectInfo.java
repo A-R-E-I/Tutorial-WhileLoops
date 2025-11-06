1858e9d758b3cbe4c4f1cc542269f34d4da8a765
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-11-06 07:57:34 -0500

    Add files via upload

--- CollectInfo.java
+++ CollectInfo.java
@@ -11,5 +11,39 @@ public class CollectInfo
 		AskInfo();
 	}
 	
+	public void AskInfo()
+	{
+		float numgrades;
+		boolean yesorno;
+		
+		System.out.print("How many grades will you be averaging?");
+		numgrades = allinput.nextFloat();
+		yesorno = CheckNum(numgrades);
+		System.out.print(yesorno);
+	}
 	
+	public boolean CheckNum(float gradenum)
+	{
+		float roundyes;
+		boolean boolstate = true;
+		
+		float isyes = gradenum % 1;
+		roundyes = Math.ceil(isyes * 10)/10;
+		System.out.print(roundyes);
+		if(isyes > 0)
+		{
+			return false;
+		}
+		else
+		{
+			if(gradenum < 1 || gradenum >= 100)
+			{
+				return false;
+			}	
+			else
+			{
+				return true;
+			}
+		}
+	}
 }
