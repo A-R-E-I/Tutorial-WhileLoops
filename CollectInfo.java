b3f3a02e7da2de0da69693fd53ea773da9a70fae
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-11-12 18:10:43 -0500

    Add files via upload

--- CollectInfo.java
+++ CollectInfo.java
@@ -6,7 +6,7 @@ public class CollectInfo //Will collect the number of grades to be inputed and c
 {
 	static Scanner allinput = new Scanner(System.in);
 	Calculate calcinfo = new Calculate();
-	
+	int yn;
 	public CollectInfo()
 	{
 		AskInfo();
@@ -30,24 +30,27 @@ public class CollectInfo //Will collect the number of grades to be inputed and c
 	public boolean CheckNum(float gradenum)
 	{
 		float roundyes;
-		boolean boolstate = true;
-		
 		
 		float isyes = gradenum % 1;
 		roundyes = (float) (Math.ceil(isyes * 10)/10);
 		System.out.print(roundyes + "\n");
 		if(isyes > 0)
 		{
+			System.out.print("\ntry again\n");
+			AskInfo();
 			return false;
 		}
 		else
 		{
 			if(gradenum < 1 || gradenum >= 100)
 			{
+				System.out.print("\ntry again\n");
+				AskInfo();
 				return false;
 			}	
 			else
 			{
+				
 				return true;
 			}
 		}
